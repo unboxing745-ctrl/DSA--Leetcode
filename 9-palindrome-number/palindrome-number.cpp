8fd816ab1d902e8697199fc463026c190dd7942c
MUSKAN KHAN <unboxing745@gmail.com>
2026-02-17 22:58:04 +0530

    Time: 6 ms (17.22%) | Memory: 8.5 MB (65.65%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     bool isPalindrome(int x) {
 
-        if (x < 0 || (x % 10 == 0 && x != 0))
-            return false;
+        if (x < 0) return false;
 
-        int reversedHalf = 0;
+        int original = x;
+        long long sum = 0;   // changed here
 
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
+        while (x != 0) {
+            int r = x % 10;
+            sum = sum * 10 + r;
+            x = x / 10;
         }
 
-        return (x == reversedHalf || x == reversedHalf / 10);
+        return sum == original;
     }
 };
